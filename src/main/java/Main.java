fda6e2f65374a8c32d3ceb1404c61521f9add04d
dheeraj.sachan@aurusnet.com <dheeraj@Y9198>
2015-02-27 11:49:41 +0530

    encrption added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,37 @@ import java.net.URL;
 
 public class Main {
 
-
+    private static void encrypt(String src,String dst)throws IOException{
+        final byte[] buf = new byte[1024];
+        byte test;
+        final InputStream is = new FileInputStream(src);
+        final OutputStream os = new FileOutputStream(dst);
+        while (true) {
+            int n = is.read(buf);
+            if (n == -1) break;
+            for(int k =0;k<n;k++) {
+                test = buf[k];
+                if(test != -128) {
+                    buf[k] = (byte)~test;
+                }
+            }
+            os.write(buf, 0, n);
+        }
+        os.close();
+        is.close();
+    }
+    
+    private static void deleteFile(String src){
+        File file = new File(src);
+        if(file.delete()){
+            System.out.println("file deleted successfully : "+file);
+        }else{
+            System.err.println("unable to delete file : " + file);
+        }
+        
+    }
+    
+    
     private static void downloadFile(URL url,File file){
         try {
             if(file.exists()){
@@ -35,7 +65,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        downloadFile(new URL("http://54.86.202.143:1935/vod_android/mp4:sample.mp4/manifest.mpd"), new File(folder + "manifest.mpd"));
+        File file = new File(folder+"manifestE.mpd");
+        if(!file.exists()) {
+            downloadFile(new URL("http://54.86.202.143:1935/vod_android/mp4:sample.mp4/manifest.mpd"), new File(folder + "manifest.mpd"));
+        }else{
+            System.out.println("manifest already exists , decrypting and using the same");
+            encrypt(folder + "manifestE.mpd",folder + "manifest.mpd");
+        }
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(folder + "manifest.mpd")));
         StringBuilder stringBuilder = new StringBuilder();
         String test;
@@ -43,6 +79,11 @@ public class Main {
             stringBuilder.append(test);
         }
         test = stringBuilder.toString();
+        bufferedReader.close();
+        
+        encrypt(folder + "manifest.mpd",folder + "manifestE.mpd");
+        deleteFile(folder+"manifest.mpd");
+        
         //System.out.println(test);
         try {
             JSONObject xmlJSONObj = XML.toJSONObject(test);
