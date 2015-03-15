2fd0a3504b0068bca80e7d3477e9dfb94864e1bc
dheeraj.sachan <dheeraj.sachan@aurusnet.com>
2015-03-16 00:12:51 +0530

    url

--- src/test/java/HttpLength.java
+++ src/test/java/HttpLength.java
@@ -9,7 +9,11 @@ public class HttpLength {
     public static void main(String[] args) throws IOException {
         URL url = new URL("http://frontend.test.superprofs.com:1935/vod_android/_definst_/mp4:sp_high_4.mp4/chunk_ctvideo_cfm4s_ridp0a0r0_cs12614400_1287793103_mpd.m4s");
         URLConnection urlConnection = url.openConnection();
+        urlConnection.connect();
         System.out.println(urlConnection.getContentLengthLong());
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
         //urlConnection.getInputStream().skip()
     }
 }
